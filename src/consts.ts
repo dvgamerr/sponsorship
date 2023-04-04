cc13c462eb64a03b7243e1dbdc3f8dba6731a490
Kananek Thongkam <info.dvgamer@gmail.com>
2023-04-04 23:11:30 +0700

    chore: component

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sponsor to me'
+export const SITE_DESCRIPTION = 'Become a sponsor to Kananek Thongkam'
